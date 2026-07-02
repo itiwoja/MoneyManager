b76d3ef46ba9c42e3de7cdf30faf21c241d5df9e
itiwoja <1kki.mrkm22@gmail.com>
2026-07-02 23:04:34 +0900

    Disable service worker caching

--- sw.js
+++ sw.js
@@ -1,36 +1,11 @@
-const CACHE_NAME = "money-manager-cache-v3";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./entry.html",
-  "./data.html",
-  "./styles.css",
-  "./app.js",
-  "./manifest.webmanifest",
-  "./icon.svg",
-];
-
 self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
-  self.skipWaiting();
+  event.waitUntil(self.skipWaiting());
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+    caches.keys().then((keys) => Promise.all(keys.map((key) => caches.delete(key))))
   );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") return;
-  event.respondWith(
-    caches
-      .match(event.request)
-      .then((cached) => cached || fetch(event.request).then((response) => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
-        return response;
-      }).catch(() => caches.match("./index.html")))
-  );
+  event.waitUntil(self.clients.claim());
+  event.waitUntil(self.registration.unregister());
 });
