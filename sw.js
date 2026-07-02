b5eb3f31096ae9790edc5ef551ab99c200bf8f01
村上壱基 <1kki.mrkm22@gmail.com>
2026-07-02 14:13:10 +0900

    fix: invalidate service worker cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "money-manager-cache-v1";
+const CACHE_NAME = "money-manager-cache-v2";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
