87599dea110bfb84aad82e3179ccade0f39acaab
itiwoja <1kki.mrkm22@gmail.com>
2026-07-02 22:58:24 +0900

    Bump service worker cache

--- sw.js
+++ sw.js
@@ -1,5 +1,14 @@
-const CACHE_NAME = "money-manager-cache-v2";
-const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
+const CACHE_NAME = "money-manager-cache-v3";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./entry.html",
+  "./data.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./icon.svg",
+];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
@@ -16,10 +25,12 @@ self.addEventListener("activate", (event) => {
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
   event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
-      const copy = response.clone();
-      caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
-      return response;
-    }).catch(() => caches.match("./index.html")))
+    caches
+      .match(event.request)
+      .then((cached) => cached || fetch(event.request).then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      }).catch(() => caches.match("./index.html")))
   );
 });
